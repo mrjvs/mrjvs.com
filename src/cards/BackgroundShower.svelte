72bdd37bb15b730d31a3a847dfa714c52caa2fd7
Jelle van Snik <jellevs@gmail.com>
2020-10-31 21:02:58 +0100

    Give button some personality

--- src/cards/BackgroundShower.svelte
+++ src/cards/BackgroundShower.svelte
@@ -9,6 +9,7 @@
 
   export let backgroundShape = "";
   export let backgroundColor = "";
+  let hitCount = 0;
 </script>
 
 <div class="card">
@@ -24,7 +25,23 @@
   {:else}
     <h2>A cool background for you :)</h2>
     <p>I made a nice looking background for you, press the button to receive it!</p>
-    <button on:click={() => setBackground()}>Claim your background</button>
+    <button on:click={() => { hitCount++; setBackground()}} class:c-gray={hitCount >= 250} class:c-red={hitCount >= 150}>
+      {#if hitCount >= 1000}
+        (why are you still clicking?)
+      {:else if hitCount >= 250}
+        Great, You've broken it :/
+      {:else if hitCount >= 100}
+        Jesus christ, you hit it {hitCount} times!?
+      {:else if hitCount >= 50}
+        This is pretty ridiculous, 50 times?
+      {:else if hitCount >= 10}
+        Really! only works once!
+      {:else if hitCount >= 2}
+        You know, the button only works once.
+      {:else}
+        Claim your background
+      {/if}
+    </button>
   {/if}
 </div>
 
@@ -48,7 +65,25 @@
     background-color: #2b3db4;
   }
 
-  button:active {
+  button:active:not(.c-gray) {
     transform: scale(1.05);
+  }
+
+  button.c-red {
+    background-color: #b43445;
+  }
+
+  button.c-red:hover {
+    background-color: #a12c3c;
+  }
+
+  button.c-gray {
+    background-color: #2f2d36;
+    transform: translateY(-0.2rem) rotate(4deg);
+    transform-origin: top left;
+  }
+
+  button.c-gray:hover {
+    background-color: #3b3942;
   }
 </style>
